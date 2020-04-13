f3208ec5a6b01daa76e5e90add825dcdfcf2a492
Taufiqul Islam <tosunbd@gmail.com>
2020-04-13 23:08:25 +0600

    Updated admin to support custom user model.

--- app/core/models.py
+++ app/core/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                         PermissionsMixin
 
-# Create your models here.
-
 class  UserManager(BaseUserManager):
 
     def create_user(self, email, password = None, **extra_fields):
