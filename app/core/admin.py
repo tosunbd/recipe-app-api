cfdb29704e9000ac6d05ad530a3cd00fbfaae577
Taufiqul Islam <tosunbd@gmail.com>
2020-04-16 20:08:50 +0600

    Configured postgres db.

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext as _
-#
-#
+
 from core import models
-#
-#
+
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
